418e904ac545d3a79f400257142a00dc764c6a91
Renato Lombardero <carlos.lombardero@isobar.com.br>
2017-11-25 20:16:15 -0200

    Boilerplate Demo

--- demo/main.js
+++ demo/main.js
@@ -4,6 +4,33 @@ var frame = 1;
 window.addEventListener('load', init, false);
 
 function init() {
+    var container = document.getElementById('world');
+
+    scene = new THREE.Scene();
+
+    WIDTH = window.innerWidth;
+    HEIGHT = window.innerHeight;
+
+    camera = new THREE.OrthographicCamera(
+        WIDTH / -2,
+        WIDTH / 2,
+        HEIGHT / 2,
+        HEIGHT / -2,
+        1, 2000
+    );
+
+    camera.position.z = 1000;
+
+    renderer = new THREE.WebGLRenderer({
+        alpha: true,
+        preserveDrawingBuffer: true,
+        antialias: true
+    });
+
+    renderer.setSize(WIDTH, HEIGHT);
+    renderer.setPixelRatio(window.devicePixelRatio ? window.devicePixelRatio : 1);
+
+    container.appendChild(renderer.domElement);
 
 
     loop();
